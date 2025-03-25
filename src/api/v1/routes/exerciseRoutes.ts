d41d494348f41afb9f5c0e92a7ca2918f909272a
Nick Gowler <ngowler@academic.rrc.ca>
2025-03-25 13:43:38 -0500

    Set up project routes.

---
+++ src/api/v1/routes/exerciseRoutes.ts
@@ -0,0 +1,9 @@
+import express, { Router } from "express";
+
+const router: Router = express.Router();
+
+router.post("/");
+router.get("/");
+router.get("/:id");
+router.put("/:id");
+router.delete("/:id");
---
+++ src/api/v1/routes/userRoutes.ts
@@ -0,0 +1,10 @@
+import express, { Router } from "express";
+
+const router: Router = express.Router();
+
+router.post("/");
+router.get("/");
+router.get("/:id");
+router.put("/:id");
+router.delete("/:id");
+router.post("/:id/upgrade");
