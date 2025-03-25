55909a5527a9845f8f2860660943b79ffd636d5d
Nick Gowler <ngowler@academic.rrc.ca>
2025-03-25 09:11:47 -0500

    Create user schema.

---
+++ src/api/v1/models/userModel.ts
@@ -0,0 +1,24 @@
+export type User = {
+    id?: string;
+    name: string;
+    email: string;
+    role: "Lite" | "Premium" | "Trainer" | "Admin";
+    healthMetrics: {
+      weight: number;
+      height: number;
+      bodyFatPercentage?: number;
+      injuriesOrLimitations?: string[];
+    };
+    workoutPreferences: {
+      daysAvailable: string[];
+      timePerDay: number;
+      gymAccess: boolean;
+      equipment?: string[];
+    };
+    background: {
+      experience: string;
+      routine: string;
+      goals: string;
+    };
+  };
+  
