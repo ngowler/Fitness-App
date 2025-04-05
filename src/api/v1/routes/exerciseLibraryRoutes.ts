1c808c5890c369a4a989390dd8069f0e8146a8df
Nick Gowler <ngowler@academic.rrc.ca>
2025-04-05 05:24:22 -0500

    Create exercise library validation.

--- src/api/v1/routes/exerciseLibraryRoutes.ts
+++ src/api/v1/routes/exerciseLibraryRoutes.ts
@@ -1,16 +1,31 @@
 import express, { Router } from "express";
+import {
+    createExercise,
+    getAllExercises,
+    getExerciseById,
+    updateExercise,
+    deleteExercise,
+    } from "../controllers/exerciseLibraryController"
+import { validateRequest } from "../middleware/validate";
+import {
+    postExerciseLibrarySchema,
+    getFilteredExercisesSchema,
+    getExerciseLibraryByIdSchema,
+    putExerciseLibrarySchema,
+    deleteExerciseLibrarySchema, }
+    from "../validations/exerciseLibraryValidation";
 
 const router: Router = express.Router();
 
 // Create new exercises for the global library
-router.post("/");
+router.post("/", validateRequest(postExerciseLibrarySchema), createExercise);
 // Retrieve details of all exercises (implement query params for filtering)
-router.get("/");
+router.get("/", validateRequest(getFilteredExercisesSchema), getAllExercises);
 // Retrieve details of a specific exercise
-router.get("/:id");
+router.get("/:id", validateRequest(getExerciseLibraryByIdSchema), getExerciseById);
 // Update exercises in the library
-router.put("/:id");
+router.put("/:id", validateRequest(putExerciseLibrarySchema), updateExercise);
 // Remove an exercise from the library
-router.delete("/:id");
+router.delete("/:id", validateRequest(deleteExerciseLibrarySchema), deleteExercise);
 
 export default router;
