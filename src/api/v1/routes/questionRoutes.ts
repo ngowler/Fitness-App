522f11014b6e2b183a087525b5e86982b0eddae2
Nick Gowler <ngowler@academic.rrc.ca>
2025-04-05 06:49:21 -0500

    Create question validation.

--- src/api/v1/routes/questionRoutes.ts
+++ src/api/v1/routes/questionRoutes.ts
@@ -1,14 +1,27 @@
 import express, { Router } from "express";
+import {
+    createQuestion,
+    getAllQuestions,
+    getQuestionById,
+    respondToQuestion,
+    } from "../controllers/questionController"
+import { validateRequest } from "../middleware/validate";
+import {
+    postQuestionSchema,
+    getQuestionsByUserSchema,
+    getQuestionByIdSchema,
+    putQuestionSchema,
+    } from "../validations/questionValidation";
 
 const router: Router = express.Router();
 
 // Submit a new question to a trainer
-router.post("/");
+router.post("/", validateRequest(postQuestionSchema), createQuestion);
 // Retrieve all questions asked by the authenticated user
-router.get("/");
+router.get("/", validateRequest(getQuestionsByUserSchema), getAllQuestions);
 // Retrieve a specific question by ID
-router.get("/:id");
+router.get("/:id", validateRequest(getQuestionByIdSchema), getQuestionById);
 // Respond to a question (Trainer)
-router.put("/:id");
+router.put("/:id", validateRequest(putQuestionSchema), respondToQuestion);
 
 export default router;
