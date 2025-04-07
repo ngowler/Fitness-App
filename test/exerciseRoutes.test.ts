3ced2a6595b623b0e15aad428be3bcc31e11c2c1
Nick Gowler <ngowler@academic.rrc.ca>
2025-04-06 23:53:37 -0500

    Finalize reviewdog errors.

--- test/exerciseRoutes.test.ts
+++ test/exerciseRoutes.test.ts
@@ -23,7 +23,7 @@ jest.mock("../src/api/v1/middleware/authenticate", () =>
 jest.mock("../src/api/v1/middleware/authorize", () =>
     jest.fn(({ hasRole }: { hasRole: string[] }) =>
         (req: Request, res: Response, next: NextFunction): void | Response => {
-            const userRole = req.headers["x-roles"];
+            const userRole: string | string[] | undefined = req.headers["x-roles"];
 
             if (Array.isArray(userRole)) {
                 if (!userRole.some(role => hasRole.includes(role))) {
